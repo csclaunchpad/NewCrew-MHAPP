81f1ec372f4ae8be04bdc7c8022e4fc3329457d3
jwams <justin.wams@csc-scc.gc.ca>
2017-09-12 10:13:49 -0700

    Update connect.php
    Removed absolute path for DB

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
 <?php
 	// Load SQL DB
-	$directory = 'sqlite:C:\Users\wamsjd\Desktop\zen\WebContent\sql\zen.db';
+	$directory = 'sqlite:..\sql\zen.db';
 	$db = new PDO($directory) or die("Cannot open the database");
 ?>
